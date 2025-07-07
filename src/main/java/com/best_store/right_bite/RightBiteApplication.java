89b9fbcefe52d699a67e72a644ab7c8b010891cb
Ihor Murashko <i.murashko0911@gmail.com>
2025-07-08 01:09:31 +0200

    Initial project structure: pom.xml, .gitignore, base folders

---
+++ src/main/java/com/best_store/right_bite/RightBiteApplication.java
@@ -0,0 +1,13 @@
+package com.best_store.right_bite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RightBiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RightBiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/best_store/right_bite/RightBiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.best_store.right_bite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RightBiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
