de6b18b4444d8c242983eccf1560cf6b8038d7aa
Evgeny Loshonkov <tixanuiop@gmail.com>
2025-07-20 16:27:33 +0300

    added base swagger info

--- src/main/java/com/best_store/right_bite/controller/catalog/CatalogController.java
+++ src/main/java/com/best_store/right_bite/controller/catalog/CatalogController.java
@@ -7,12 +7,18 @@ import com.best_store.right_bite.dto.catalog.ProductDTO;
 import com.best_store.right_bite.dto.catalog.ProductFilterRequest;
 
 import com.best_store.right_bite.service.catalog.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(
+        name = "Catalog contorller",
+        description = "it is letting get information about product, its brands, categories and filter by parameters - price, desk, ask, brand, category."
+)
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/catalog")
@@ -23,11 +29,20 @@ public class CatalogController {
     private final BrandService brandService;
 
     @GetMapping()
+    @Operation(
+            summary = "getting all products without filters",
+            description = "no parameters, default realization of getting all products"
+    )
     public List<ProductDTO> getAllProduct()
     {
         return catalogService.getAllProduct();
     }
 
+
+    @Operation(
+            summary = "getting all products with filters",
+            description = "parametrized, "
+    )
     @PostMapping("/filter")
     public Page<ProductDTO> getAllProduct(
             @RequestBody(required = false) ProductFilterRequest productFilterRequest)
--- src/main/java/com/best_store/right_bite/dto/catalog/ProductFilterRequest.java
+++ src/main/java/com/best_store/right_bite/dto/catalog/ProductFilterRequest.java
@@ -1,25 +1,36 @@
 package com.best_store.right_bite.dto.catalog;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
+@Schema(description = "The object of product filtering")
 public class ProductFilterRequest {
 
+    //TODO fix problem with category name
     private String categoryName;
 
+    @Schema(description = "brand of product", example = "Biona")
     private String brand;
 
+    @Schema(description = "range price from ", example = "10")
     private BigDecimal priceFrom;
+
+    @Schema(description = "range price to ", example = "25")
     private BigDecimal priceTo;
 
+    @Schema(description = "sort by first letter", example = "a")
     private Character aZ;
 
+    @Schema(description = "sort by parameters", example = "priceAsc, priceDesc, rating, popular")
     private String sortBy; // "priceAsc", "priceDesc", "rating", "popular"
 
-
+    @Schema(description = "Number page", example = "2")
     private int page = 0;
+
+    @Schema(description = "count of items in page", example = "10")
     private int size = 24;
 
 }
