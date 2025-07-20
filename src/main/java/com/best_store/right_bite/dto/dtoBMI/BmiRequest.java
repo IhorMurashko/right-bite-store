722f6143e20acd881098d644c4dbeb650c645add
Nikita <75564714+nikitaazz@users.noreply.github.com>
2025-07-20 12:55:19 +0200

    swagger_documentation

--- src/main/java/com/best_store/right_bite/dto/dtoBMI/BmiRequest.java
+++ src/main/java/com/best_store/right_bite/dto/dtoBMI/BmiRequest.java
@@ -2,6 +2,7 @@ package com.best_store.right_bite.dto.dtoBMI;
 
 
 import com.best_store.right_bite.constans.Gender;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
@@ -17,16 +18,20 @@ public record BmiRequest(
         @NotNull
         @Min(value = 2, message = "Age must be greater than 1 year")
         @Max(value = 120, message = "Age must be less than 120 years")
+        @Schema(description = "Users age", example = "18", accessMode = Schema.AccessMode.READ_ONLY)
         int age,
         @NotNull
+        @Schema(description = "Choose gender using radiobutton", example = "MALE", accessMode = Schema.AccessMode.READ_ONLY)
         Gender gender,
         @NotNull
         @Min(value = 2, message = "Weight must be greater than 1 kg")
         @Max(value = 250, message = "Weight must be less than 250 kg")
+        @Schema(description = "Users weight in kg", example = "77.0", accessMode = Schema.AccessMode.READ_ONLY)
         double weightKg,
         @NotNull
         @Min(value = 2, message = "Height must be greater than 1 cm")
         @Max(value = 250, message = "Height must be less than 250 cm")
+        @Schema(description = "Users height in cm", example = "184.0", accessMode = Schema.AccessMode.READ_ONLY)
         double heightCm
 ) {
 }
