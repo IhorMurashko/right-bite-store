d879110d709064146a76eb6c40d8f61c2548fde3
Ihor Murashko <i.murashko0911@gmail.com>
2025-09-06 14:10:43 +0200

    resolved conflicts

---
+++ src/main/java/com/best_store/right_bite/service/order/domain/OrderService.java
@@ -0,0 +1,27 @@
+package com.best_store.right_bite.service.order.domain;
+
+import com.best_store.right_bite.constant.order.OrderStatus;
+import com.best_store.right_bite.model.order.Order;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+public interface OrderService {
+    Optional<Order> findById(@NotNull Long id);
+
+    List<Order> findAllByUserId(@NotNull Long userId);
+
+    Order save(@NotNull Order order);
+
+    List<Order> findByStatus(@NotNull OrderStatus status);
+
+    List<Order> findAll();
+
+    void deleteById(@NotNull Long id);
+
+    Long totalCountOrders();
+
+    BigDecimal totalPriceOrders();
+}
